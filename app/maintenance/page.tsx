a50a1cd7d883bc6974efb2b8afc3b29298576c5a
bakar-bkr-studio <bakar77100@gmail.com>
2026-05-08 21:55:19 +0200

    fix: correction adresse mail sur page maintenance

--- app/maintenance/page.tsx
+++ app/maintenance/page.tsx
@@ -319,7 +319,7 @@ export default function MaintenancePage() {
               <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/>
             </svg>
             <span>Une question ?&nbsp;</span>
-            <a href="mailto:contact@afia-beauval.fr">contact@afia-beauval.fr</a>
+            <a href="mailto:famillesdicietdailleurs@gmail.com">famillesdicietdailleurs@gmail.com</a>
           </div>
         </div>
       </div>
